8e13cdfa881c0de1820a9f55650389466006a0dd
milos-rakita <milossrb.rakita@gmail.com>
2016-12-05 13:29:54 +0100

    d

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,3 +1,41 @@
 var redux = require('redux');
 
 console.log('starting redux example');
+
+
+// //pure funcction
+// function add(a,b){
+//     return a+b;
+// }
+
+// var a = 3;
+// function add(b){
+//     return a+b;
+// }
+
+// var result;
+// function add(a,b){
+//     result =a+b;
+//     return result;
+// }
+
+// function add(a,b){
+//     return a+b+ new Date().getSeconds;
+// }
+
+// function changeProp(obj){
+//     return {
+//         ...obj,
+//         name: 'Jen'
+//     };
+//     // obj.name = 'jen';
+//     // return obj;
+// }
+
+// var startingCalue = {
+//     name: 'andrew',
+//     age: 25
+// };
+// var res = changeProp(startingCalue);
+// console.log(startingCalue);
+// console.log(res);
