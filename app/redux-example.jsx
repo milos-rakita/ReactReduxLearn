48a0828ffe478e0df0bf9de2af082d97c41d2646
milos-rakita <milossrb.rakita@gmail.com>
2016-12-06 15:05:53 +0100

    d

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,14 @@ var redux = require('redux');
 
 console.log('starting redux example');
 
-var reducer = (state = {name: 'Anonymous'},action) => {
+var stateDefault={
+    name: 'Anonymous',
+    hobbies: [],
+    movies: []
+};
+var nextHobbyId =1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault,action) => {
     // state = state || {name: 'Anonymous'};
     
     console.log('new action ',action);
@@ -11,8 +18,41 @@ var reducer = (state = {name: 'Anonymous'},action) => {
         case 'CHANGE_NAME':
             return{
                 ...state,
-                searchText:action.searchText
+                searchText: action.name
             };
+        case 'ADD_HOBBY':
+            return{
+                ...state,
+                hobbies: [
+                    ...state.hobbies,
+                    {
+                        id: nextHobbyId++,
+                        hobby: action.hobby
+                    }
+                ]
+            };
+        case 'ADD_MOVIE':
+            return{
+                ...state,
+                movies: [
+                    ...state.movies,
+                    {
+                        id: nextMovieId++,
+                        title: action.title,
+                        genre: action.genre
+                    }
+                ]
+            }
+        case 'REMOVE_HOBBY':
+            return{
+                ...state,
+                hobbies:state.hobbies.filter((hobby)=>hobby.id !== action.id)
+            }
+        case 'REMOVE_MOVIE':
+            return{
+                ...state,
+                movies:state.movies.filter((movie)=> movie.id !== action.id)
+            }
         dafault:
             return state;
     }
@@ -28,6 +68,8 @@ var unsubscribe = store.subscribe(()=>{
 
     console.log('name is ',state.name);
     document.getElementById('app').innerHTML=state.name;
+
+    console.log('NEw state ',store.getState());
 });
 //unsubscribe();
 
@@ -40,8 +82,34 @@ store.dispatch({
 });
 
 store.dispatch({
+    type:'ADD_HOBBY',
+    hobby: 'Running'
+});
+
+store.dispatch({
+    type: 'REMOVE_HOBBY',
+    id: 2
+});
+
+store.dispatch({
     type:'CHANGE_NAME',
     name: 'emily'
 });
 
-console.log('name should be andrew',store.getState());
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'MAd MAx',
+    gender: 'Action'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'lajanje na zvezde',
+    gender: 'Action'
+});
+
+store.dispatch({
+    type: 'REMOVE_MOVIE',
+    id: 1
+})
+// console.log('name should be andrew',store.getState());
